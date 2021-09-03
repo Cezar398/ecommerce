59408c6c4b9e3333cea85dc71cb10b449c54f75b
Cezar <cezar397@Cezars-MacBook-Pro.local>
2021-09-03 02:19:29 +0200

    Update

--- api/app.py
+++ api/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, escape, request
-
+from flask_cors import CORS
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/')
 def welcome():
     return "<h1 style='text-align: center'>ECOMMERCE API</h1>"
